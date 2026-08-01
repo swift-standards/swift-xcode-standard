8faac7c17709dbcb1eccffa0aa81c5cc929d574a
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-01 03:05:00 +0200

    Apply lint fix: minimal-type-body member motion

--- Sources/Xcode Workspace Standard/Xcode.Workspace.Location.swift
+++ Sources/Xcode Workspace Standard/Xcode.Workspace.Location.swift
@@ -5,25 +5,27 @@ extension Xcode.Workspace {
         case group(Swift.String)
         case absolute(Swift.String)
         case `self`
+    }
+}
 
-        public var rawValue: Swift.String {
-            switch self {
-            case .group(let path): "group:\(path)"
-            case .absolute(let path): "absolute:\(path)"
-            case .self: "self:"
-            }
+extension Xcode.Workspace.Location {
+    public var rawValue: Swift.String {
+        switch self {
+        case .group(let path): "group:\(path)"
+        case .absolute(let path): "absolute:\(path)"
+        case .self: "self:"
         }
+    }
 
-        public init?(rawValue: Swift.String) {
-            if rawValue == "self:" {
-                self = .self
-            } else if rawValue.hasPrefix("group:") {
-                self = .group(Swift.String(rawValue.dropFirst(6)))
-            } else if rawValue.hasPrefix("absolute:") {
-                self = .absolute(Swift.String(rawValue.dropFirst(9)))
-            } else {
-                return nil
-            }
+    public init?(rawValue: Swift.String) {
+        if rawValue == "self:" {
+            self = .self
+        } else if rawValue.hasPrefix("group:") {
+            self = .group(Swift.String(rawValue.dropFirst(6)))
+        } else if rawValue.hasPrefix("absolute:") {
+            self = .absolute(Swift.String(rawValue.dropFirst(9)))
+        } else {
+            return nil
         }
     }
 }
